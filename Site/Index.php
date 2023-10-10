25c76ddf16012493ec60bfeebf7985b0b1db1c57
PatrickSkrebel <peskrebel@email.neit.edu>
2023-10-10 14:51:49 -0400

    site page

--- Site/Index.php
+++ Site/Index.php
@@ -1,3 +1,5 @@
+<?php include __DIR__ . '/../include/header.php'; ?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -6,8 +8,25 @@
     <title>Document</title>
 </head>
 <body>
-    <li>
-        <a href="week1/fizzBuzz.php">Fizz Buzz</a>
-    </li>
+    <h1>PHP, MySQL, and More! - Douglas Rose</h1>
+    <p>
+    Welcome to my PHP and MySQL page. You can find an overview of all my working PHP projects along with my code.
+    </p>
+
+
+    <h2>Assignment OverView</h2>
+
+    <ul>
+        <li><a href="../week1/fizzBuzz.php">Week 1</li>
+        <li><a href="../week2/fizzBuzz.php">Week 2</li>
+        <li><a href="../week3/fizzBuzz.php">Week 3</li>
+        <li><a href="../week4/fizzBuzz.php">Week 4</li>
+        <li><a href="../week5/fizzBuzz.php">Week 5</li>
+        <li><a href="../week6/fizzBuzz.php">Week 6</li>
+        <li><a href="../week7/fizzBuzz.php">Week 7</li>
+        <li><a href="../week8/fizzBuzz.php">Week 8</li>
+        <li><a href="../week9/fizzBuzz.php">Week 9</li>
+        <li><a href="../week10/fizzBuzz.php">Week 10</li>
+    </ul>
 </body>
 </html>
