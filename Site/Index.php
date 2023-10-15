7c6672dd03c887c08cf36beca0b92e8bb17bbc60
PatrickSkrebel <peskrebel@email.neit.edu>
2023-10-15 14:40:45 -0400

    change

--- Site/Index.php
+++ Site/Index.php
@@ -8,7 +8,7 @@
     <title>Document</title>
 </head>
 <body>
-    <h1>PHP, MySQL, and More! - Douglas Rose</h1>
+    <h1>PHP, MySQL, and More! - Patrick Skrebel</h1>
     <p>
     Welcome to my PHP and MySQL page. You can find an overview of all my working PHP projects along with my code.
     </p>
@@ -18,15 +18,20 @@
 
     <ul>
         <li><a href="../week1/fizzBuzz.php">Week 1</li>
-        <li><a href="../week2/fizzBuzz.php">Week 2</li>
-        <li><a href="../week3/fizzBuzz.php">Week 3</li>
-        <li><a href="../week4/fizzBuzz.php">Week 4</li>
-        <li><a href="../week5/fizzBuzz.php">Week 5</li>
-        <li><a href="../week6/fizzBuzz.php">Week 6</li>
-        <li><a href="../week7/fizzBuzz.php">Week 7</li>
-        <li><a href="../week8/fizzBuzz.php">Week 8</li>
-        <li><a href="../week9/fizzBuzz.php">Week 9</li>
-        <li><a href="../week10/fizzBuzz.php">Week 10</li>
+        <li><a href="../week2/index.php">Week 2</li>
+        <li><a href="../week3/index.php">Week 3</li>
+        <li><a href="../week4/index.php">Week 4</li>
+        <li><a href="../week5/index.php">Week 5</li>
+        <li><a href="../week6/index.php">Week 6</li>
+        <li><a href="../week7/index.php">Week 7</li>
+        <li><a href="../week8/index.php">Week 8</li>
+        <li><a href="../week9/index.php">Week 9</li>
+        <li><a href="../week10/index.php">Week 10</li>
     </ul>
+
+    <footer>
+    <?php include __DIR__ . '/../include/footer.php'; ?>
+</footer>
+
 </body>
 </html>
